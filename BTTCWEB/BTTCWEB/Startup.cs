81ee6aaf40fd243f6344c4c0754999d97f337c28
cheney18 <chenliangfeng1@163.com>
2014-11-12 11:25:02 +0800

    Revert "Revert "代码""
    This reverts commit 502f7c54fdf3c42846dcceae7dd0eb320c55ef11.

---
+++ BTTCWEB/BTTCWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BTTCWEB.Startup))]
+namespace BTTCWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
